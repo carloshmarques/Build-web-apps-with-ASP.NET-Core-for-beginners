df4a2172ea8ae0a5921427179231c7572fe8f58e
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-12 16:34:57 +0000

    commit

--- mslearn-create-razor-pages-aspnet-core/ContosoPizza/obj/Debug/net7.0/ContosoPizza.AssemblyInfo.cs
+++ mslearn-create-razor-pages-aspnet-core/ContosoPizza/obj/Debug/net7.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f57b68f1de2657fba042322783dbef70cd2fd8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b643431c327c2b7e155b5634bfb654111c8563f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
